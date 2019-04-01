eff6df93a930229ec63736a78aa486626d872589
phpshahariar <shahariar.ikbal86@gmail.com>
2019-04-02 02:10:13 +0600

    update

--- resources/views/users/borders-dashboard.blade.php
+++ resources/views/users/borders-dashboard.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
     <div class="container-fluid">
-
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#borderAddModal">
+            <i class="fas fa-plus-circle"> Add Meal</i>
+        </button>
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">{{ Auth::user()->name }} Your Account</h1>
@@ -11,15 +13,25 @@
 
         <!-- Content Row -->
         <div class="row">
-
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-primary shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Meal</div>
+                                @php
+                                    $sum = 0
+                                @endphp
+                                @foreach(Auth::user()->Border as $key => $data)
+                                    <tr>
+                                        <td>
+                                            {!! $total = $data->breakfast+$data->lunch+$data->dinner !!}
+                                        </td>
+                                    </tr>
+                                    @php($sum = $sum+$total)
+                                @endforeach
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $sum }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -35,8 +47,8 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Deposit Balance</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">000</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -52,10 +64,10 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Expanse Balance</div>
                                 <div class="row no-gutters align-items-center">
                                     <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">0000</div>
                                     </div>
                                     <div class="col">
                                         <div class="progress progress-sm mr-2">
@@ -78,8 +90,38 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Border Pay</div>
                                 <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-comments fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Hostel Pay</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">000000</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-comments fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Meal Rate</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">0000.00</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-comments fa-2x text-gray-300"></i>
@@ -91,12 +133,55 @@
         </div>
     </div>
 
-{{--    Add Daily Meals--}}
-    <div class="container">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#borderAddModal">
-            <i class="fas fa-plus-circle"> Add Meal</i>
-        </button>
 
+{{--    Data Table--}}
+    <div class="container">
+        <h1 class="h3 mb-2 text-gray-800">Data Tables</h1>
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">DataTables</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th>SL NO</th>
+                            <th>Date</th>
+                            <th>Name</th>
+                            <th>Breakfast</th>
+                            <th>Lunch</th>
+                            <th>Dinner</th>
+                            <th>Total</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @php($sum = 0)
+                        @foreach(Auth::user()->Border as $key => $data)
+                            <tr>
+                                <td>{!! $key+1 !!}</td>
+                                <td>{!! $data->date !!}</td>
+                                <td>{!! $data->name !!}</td>
+                                <td>{!! $data->breakfast !!}</td>
+                                <td>{!! $data->lunch !!}</td>
+                                <td>{!! $data->dinner !!}</td>
+                                <td>
+                                    {!! $total = $data->breakfast+$data->lunch+$data->dinner !!}
+                                </td>
+                            </tr>
+                            @php($sum = $sum+$total)
+                        @endforeach
+                        </tbody>
+                        <div class="alert alert-heading"></div>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{--    Add Daily Meals--}}
+    <div class="container">
         <!-- Modal -->
         <div class="modal fade" id="borderAddModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
@@ -140,54 +225,7 @@
             </div>
         </div>
     </div>
-{{--    End Meal Modal--}}
-    <br/>
-{{--    Data Table--}}
-    <div class="container">
-        <h1 class="h3 mb-2 text-gray-800">Data Tables</h1>
-        <!-- DataTales Example -->
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">DataTables</h6>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                        <thead>
-                        <tr>
-                            <th>SL NO</th>
-                            <th>Date</th>
-                            <th>Name</th>
-                            <th>Breakfast</th>
-                            <th>Lunch</th>
-                            <th>Dinner</th>
-                            <th>Total</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @php($sum = 0)
-                        @foreach(Auth::user()->Border as $key => $data)
-                            <tr>
-                                <td>{!! $key+1 !!}</td>
-                                <td>{!! $data->date !!}</td>
-                                <td>{!! $data->name !!}</td>
-                                <td>{!! $data->breakfast !!}</td>
-                                <td>{!! $data->lunch !!}</td>
-                                <td>{!! $data->dinner !!}</td>
-                                <td>
-                                    {!! $total = $data->breakfast+$data->lunch+$data->dinner !!}
-                                </td>
-                            </tr>
-{{--                            @php($sum = $sum+$total)--}}
-                        @endforeach
-                        </tbody>
-                        <div class="alert alert-heading"></div>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-
+    {{--    End Meal Modal--}}
 {{--    AJAX Crud--}}
 {{--    Insert Data--}}
 {{--    <script type="text/javascript">--}}
